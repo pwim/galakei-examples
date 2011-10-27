5f55ddbe89784625e00ecc6d9254fa27f15d28fc
Paul McMahon <paul@mobalean.com>
2011-10-27 10:37:52 +0900

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GalakeiExamples::Application.config.secret_token = 'f5330948d22212ad1cb3dcde6b302bfc4392707ad92d2ee328f383bda0db603cb6a3b02e4008fa747e73879a6f9dce9315b5299c222649106ba738bde14e9735'
